8d98bbd4e29eee3f4e044fb90f6e890ed432e95e
Celia <au582514@post.au.dk>
2019-01-30 13:48:11 +0100

    Accumulator property oprettet, samt tilføjet kode i operationerne

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -8,24 +8,37 @@ namespace Calculator
 {
     public class Calculator
     {
+        public double Accumulator { get; private set; }
+
         public double Add(double a, double b)
         {
+            Accumulator = 0;
+            Accumulator = a + b;
             return a + b;
         }
 
         public double Subtract(double a, double b)
         {
+            Accumulator = 0;
+            Accumulator = a - b;
             return a - b;
         }
 
         public double Multiply(double a, double b)
         {
+            Accumulator = 0;
+            Accumulator = a * b;
             return a * b;
         }
 
         public double Power(double x, double exp)
         {
+            Accumulator = 0;
+            Accumulator = Math.Pow(x, exp);
             return Math.Pow(x, exp);
         }
+
+
+        
     }
 }
