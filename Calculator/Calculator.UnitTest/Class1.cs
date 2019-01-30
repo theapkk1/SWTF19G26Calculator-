35d2c46c39bbf8e01dd47081f88358b633f9ff8c
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2019-01-30 11:41:25 +0100

    Tilføjet Calculator.UnitTest klassen

---
+++ Calculator/Calculator.UnitTest/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculator.UnitTest
+{
+    public class Class1
+    {
+        [TestFixture]
+        public class CalculatorUnitTest
+        {
+            private Calculator uut;
+
+            [SetUp]
+            public void Setup()
+            {
+                uut = new Calculator();
+            }
+
+            [TestCase(2, 2, 4)]
+            [TestCase(-2, -2, -4)]
+            [TestCase(2.5, 2.5, 5)]
+            [TestCase(2.5, -2.5, 0)]
+            public void Add_AAndB_returnsResult(double a, double b, double result)
+            {
+                Assert.That(uut.Add(a, b), Is.EqualTo(result).Within(0.01));
+            }
+
+            [TestCase(2, 2, 0)]
+            [TestCase(-2, -2, 0)]
+            [TestCase(2.5, 1.5, 1)]
+            public void Subtract_AAndB_returnResult(double a, double b, double result)
+            {
+                Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
+            }
+
+            [TestCase(3, 3, 9)]
+            [TestCase(-3, 3, -9)]
+            public void Multiply_AAndB_returnsResult(double a, double b, double result)
+            {
+                Assert.That(uut.Multiply(a, b), Is.EqualTo(result));
+            }
+
+            [TestCase(1, 3, 1)]
+            [TestCase(-1, -3, -1)]
+            public void Power_AAndB_returnsResult(double x, double exp, double result)
+            {
+                Assert.That(uut.Power(x, exp), Is.EqualTo(result));
+            }
+
+        }
+    }
+}
